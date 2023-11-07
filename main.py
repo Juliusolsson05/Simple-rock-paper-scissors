082255f58c886a78028feb73f2b11c3c85e5708a
Juliusolsson05 <julius.olsson05@gmail.com>
2023-11-07 17:33:08 +0100

    Updated to better ASCII art and made smileys with colors depending on what result the user gets

--- main.py
+++ main.py
@@ -1,58 +1,25 @@
 import random
 import time
+from colorama import Fore, Style
+from art import *
 
-# ASCII art numbers for countdown
-ascii_numbers = {
-    '5': """
-  #####
- #     
- #     
-  #####
-      #
- #    #
-  #### 
-""",
-    '4': """
-   #  #
-  #   #
- #    #
-#######
-     #
-     #
-""",
-    '3': """
- ######
-#     #
-      #
- ######
-      #
-#     #
- ######
-""",
-    '2': """
- ######
-#     #
-      #
- #####
-#     
-#     
-#######
-""",
-    '1': """
-   ##
-  # #
-    #
-    #
-    #
-    #
-   ###
-"""
-}
+# Initialize colorama
+from colorama import init
+init(autoreset=True)
 
 def countdown():
+    # ASCII art numbers for countdown
     for number in ['5', '4', '3', '2', '1']:
-        print(ascii_numbers[number])
-        time.sleep(1)  # Wait for one second
+        print(text2art(number))  # Using the art library to generate ASCII art for numbers
+        time.sleep(1)
+
+def print_smiley(result):
+    if result == "win":
+        print(Fore.GREEN + text2art(":)", "big"))
+    elif result == "lose":
+        print(Fore.RED + text2art(":(", "big"))
+    else:  # it's a tie
+        print(Fore.YELLOW + text2art(":|", "big"))
 
 def rock_paper_scissors():
     # Ordbok för att lagra de möjliga valen
@@ -70,12 +37,15 @@ def rock_paper_scissors():
 
         # Bestäm vinnaren
         if user_choice == computer_choice:
+            print_smiley("tie")
             print("Det är lika!")
         elif (user_choice == 1 and computer_choice == 2) or \
              (user_choice == 2 and computer_choice == 3) or \
              (user_choice == 3 and computer_choice == 1):
+            print_smiley("win")
             print("Du vinner!")
         else:
+            print_smiley("lose")
             print("Du förlorar!")
     else:
         print("Ogiltigt val. Vänligen ange 1, 2, eller 3.")
